ee6629bbff8d3acbfdd99206b1708af34dfc367f
DamonSFu <475774287@qq.com>
2021-04-19 10:46:23 +0800

    test

---
+++ src/main/java/com/baizhi/entity/Info.java
@@ -0,0 +1,41 @@
+package com.baizhi.entity;
+
+public class Info {
+
+    private int id;
+    private String cardno;//身份证号
+    private String address;//地址
+
+    @Override
+    public String toString() {
+        return "Info{" +
+                "id=" + id +
+                ", cardno='" + cardno + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCardno() {
+        return cardno;
+    }
+
+    public void setCardno(String cardno) {
+        this.cardno = cardno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
